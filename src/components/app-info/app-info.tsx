e5904230aa4b6422fe226a78f5dd93f7d249b705
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-01-26 13:13:00 +0600

    AppInfo

---
+++ src/components/app-info/app-info.tsx
@@ -0,0 +1,13 @@
+import './app-info.css'
+
+const AppInfo = () => {
+    return (
+        <div className='app-info'>
+            <h1>Accounting for ABM employees</h1>
+            <h2>The total number of employees:</h2>
+            <h2>The award will be received:</h2>
+        </div>
+    )
+}
+
+export default AppInfo
